0da248b3920df4e35f92384597eb75c4f7b7a102
Kristoffer Andergrim <andergrim@gmail.com>
2023-04-03 23:00:19 +0200

    Now does what it should

--- lua/flipper/config.lua
+++ lua/flipper/config.lua
@@ -1,19 +1,58 @@
 local Flipper = {}
 
---- Your plugin configuration with its default values.
+--- Registers the plugin mappings if the option is enabled.
 ---
---- Default values:
+---@param options table The mappins provided by the user.
+---@param mappings table A key value map of the mapping name and its command.
+---
+---@private
+local function registerMappings(options, mappings)
+    -- all of the mappings are disabled
+    if not options.enabled then
+        return
+    end
+
+    for name, command in pairs(mappings) do
+        -- this specific mapping is disabled
+        if not options[name] then
+            return
+        end
+
+        assert(type(options[name]) == "string", string.format("`%s` must be a string", name))
+        vim.api.nvim_set_keymap("n", options[name], command, { silent = true })
+    end
+end
+
+--- Flipper configuration with its default values.
+---
+---@type table
+---Default values:
 ---@eval return MiniDoc.afterlines_to_code(MiniDoc.current.eval_section)
 Flipper.options = {
-    -- Prints useful logs about what event are triggered, and reasons actions are executed.
+    --- @type boolean 
     debug = false,
+    -- Defines the word pairs to flip between
+    ---@type table
     flippers = {
         {"true",    "false"},
+        {"True",    "False"},
         {"yes",     "no"},
         {"on",      "off"},
         {"enabled", "disabled"},
         {"enable",  "disable"},
-    }
+    },
+    -- Keyboard mappings.
+    --- @type table
+    mappings = {
+        -- When `true`, creates all the mappings that are not set to `false`.
+        --- @type boolean
+        enabled = false,
+        -- Sets a global mapping to Neovim which allows you to toggle the
+        -- word under the cursor
+        -- When `false`, the mapping is not created.
+        --- @type string
+        flipword = "<Leader>i",
+    },
 }
 
 --- Define your flipper setup.
@@ -25,6 +64,10 @@ function Flipper.setup(options)
     options = options or {}
 
     Flipper.options = vim.tbl_deep_extend("keep", options, Flipper.options)
+
+    registerMappings(Flipper.options.mappings, {
+        flipword = ":Flipper<CR>",
+    })
 
     return Flipper.options
 end
--- lua/flipper/main.lua
+++ lua/flipper/main.lua
@@ -5,7 +5,28 @@ local Flipper = {}
 
 ---@private
 function Flipper.flip()
-    print('Hello world')
+    local col = vim.api.nvim_win_get_cursor(0)[2]
+    local char = vim.api.nvim_get_current_line():sub(col+1,col+1)
+
+    -- Don't proceed if we're on a whitespace
+    if string.match(char, "%s") then
+        return
+    end
+
+    local current_word = vim.fn.expand('<cword>')
+
+    for i = 1, #_G.Flipper.config.flippers do
+        local word_first, word_second = unpack(_G.Flipper.config.flippers[i])
+        if word_first == current_word then
+            vim.cmd("normal! ciw" .. word_second)
+            vim.cmd("normal! b")
+            break
+        elseif word_second == current_word then
+            vim.cmd("normal! ciw" .. word_first)
+            vim.cmd("normal! b")
+            break
+        end
+    end
 end
 
 return Flipper
